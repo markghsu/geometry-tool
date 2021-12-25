156dfb11dd09febe6564f3e0a6cbf85798b41380
Mark Hsu <mark.g.hsu@gmail.com>
2021-12-24 17:48:05 -0800

    Update Boilerplate

---
+++ src/components/Polygon.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Polygon( props ) {
+    return <svg viewBox="0 0 100 100">
+        <polygon points="0,0 20,20 50,10"/>
+    </svg>
+} 
+
+export default Polygon;
